5a8c7c46c39e3ecbdea2718073b988e377f8dcaf
ksungsu <zxzxzlxlzlxl@naver.com>
2022-12-22 19:54:40 +0900

    Spring Web API

---
+++ src/main/java/com/sungsu/response/ErrorResponse.java
@@ -0,0 +1,29 @@
+package com.sungsu.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//@RequiredArgsConstructor
+@Getter
+@RequiredArgsConstructor
+public class ErrorResponse {
+
+    private final String code;
+    private final String message;
+
+    private final Map<String, String> validation = new HashMap<>();
+
+//    @Autowired
+//    public ErrorResponse(String code, String message){
+//        this.code = code;
+//        this.message = message;
+//    }
+
+    public void addValidation(String fieldName, String errorMessage){
+        this.validation.put(fieldName, errorMessage);
+    }
+}
--- src/test/java/com/sungsu/controller/PostControllerTest.java
+++ src/test/java/com/sungsu/controller/PostControllerTest.java
@@ -39,8 +39,10 @@ class PostControllerTest {
       mockMvc.perform(post("/posts")
               .contentType(MediaType.APPLICATION_JSON)
               .content("{\"title\":\"\", \"content\":\"내용입니다.\"}")
-              ).andExpect(status().isOk())
-              .andExpect(jsonPath("$.title").value("타이틀을 입력해주세요"))
+              ).andExpect(status().isBadRequest())
+              .andExpect(jsonPath("$.code").value("400"))
+              .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+              .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요"))
               .andDo(print());
     }
 
