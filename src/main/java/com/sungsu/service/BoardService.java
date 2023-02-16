9ffe49ad0f9463daf40ef398d42280386e1ae48f
ksungsu <zxzxzlxlzlxl@naver.com>
2023-02-16 22:29:13 +0900

    Spring Web API

--- src/main/java/com/sungsu/service/BoardService.java
+++ src/main/java/com/sungsu/service/BoardService.java
@@ -40,6 +40,13 @@ public class BoardService {
                 .build();
     }
 
+
+    /**
+     * 글이 너무 많은 경우 -> 비용이 너무 많이 든다.
+     * 글이 -> 100,000,000 -> DB 글 모두 조회하는 경우 -> DB가 뻗을 수 있다.
+     * DB -> 애플리케이션 서버로 전달하는 시간, 트래픽 비용 등이 많이 발생할 수 있다.
+     */
+
     public List<PostResponse> getList() {
                 return boardRepository.findAll().stream()
                 .map(PostResponse::new)
